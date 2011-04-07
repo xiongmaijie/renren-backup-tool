98bd89434b9aa49cbaf1a278a59256ac79b781be
kaisir.wang@gmail.com <kaisir.wang@gmail.com@27c7521a-fdea-5b74-e51b-bf14b2f7cd2e>
2011-04-07 03:12:13 +0000

    2011-4-7 1)改进获取与保存算法，按页获取与保存 2)支持导出“导入人人网的日志”与“手机发表的日志” 3)增加断线续传功能 4)保存时实时显示当前日志的名称，方便查阅

--- trunk/Program.cs
+++ trunk/Program.cs
@@ -8,15 +8,25 @@ namespace RenRen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("======== 人人网日志备份工具 V0.5版 20110312,Programing By Kaisir =========");
+            int startPage = 0;//开始页 为续存做准备
+            int artNo = 0; //文章编号
+            Console.WriteLine("======== 人人网日志备份工具 V0.6版 20110407,Programing By Kaisir =========");
             Console.WriteLine("================== 感谢Alin编写图片保存的模块 ============================");
             Console.WriteLine("============= 感谢使用，如有问题，可联系 Kaisir.Wang@Gmail.Com ===========");
             Console.WriteLine("======================= 我的小站 Http://Kaisir.Com ======================");
             //初始化存放目录
             Kaisir.Common.Common.createFolder(@"D:\Kaisir\");//初始化
             Kaisir.RenRen.RenRenHelper.Login();//登录
-            string html=Kaisir.RenRen.RenRenHelper.getList();//获取文章列表
-            Kaisir.RenRen.RenRenHelper.GetArt(html);//获取日志
+            int totalpage=Kaisir.RenRen.RenRenHelper.getTotalPage();//获取总页数
+            startPage=Kaisir.RenRen.RenRenHelper.loadPoint(totalpage);//读取断点文件，并根据断点文件置当前页
+            string html;
+            for (int i=startPage; i <=totalpage ; i++)//修正保存函数，每次保存一页，方便实现断线续存！
+            {
+                html = Kaisir.RenRen.RenRenHelper.getList(i);
+                artNo=Kaisir.RenRen.RenRenHelper.GetArt(html);
+                html = "";
+                Kaisir.RenRen.RenRenHelper.savePoint(totalpage, i,artNo);//为续存准备
+            }
         }
     }
 }
